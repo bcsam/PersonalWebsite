9955d9cd0b9c7fe5e292f140e22c0ca7b7791d9d
Brent Samuels <bcsam@mit.edu>
2020-08-28 20:22:55 -0400

    Finished Research Page

--- src/pages/Research.js
+++ src/pages/Research.js
@@ -15,23 +15,37 @@ export default class Research extends Component {
         <h1>Overview</h1>
         <p>
           During my time at MIT I pursued academic research opportunities
-          through my classes and programs. I participated in the Undergraduate
-          Reseach Opportunities Program (
-          <a href="https://urop.mit.edu/">UROP</a>) multiple times and then went
-          on to participate in the advanced year-long SuperUROP program for my
-          sponsored research experiences. Additionally, my Advanced
+          through my classes and programs. For my sponsored research, I
+          participated in the Undergraduate Reseach Opportunities Program (
+          <b>
+            <a style={{ color: "black" }} href="https://urop.mit.edu/">
+              UROP
+            </a>
+          </b>
+          ) multiple times and then went on to participate in the advanced
+          year-long SuperUROP program. For my projects, my Advanced
           Undergraduated Studies (
-          <a href="https://www.eecs.mit.edu/academics-admissions/undergraduate-programs/curriculum/advanced-undergraduate-subjects">
-            AUS
-          </a>
-          )
+          <b style={{ color: "black" }}>
+            <a
+              style={{ color: "black" }}
+              href="https://www.eecs.mit.edu/academics-admissions/undergraduate-programs/curriculum/advanced-undergraduate-subjects"
+            >
+              AUS
+            </a>
+          </b>
+          ) gave opportunities to create extended projects near the end of the
+          course.
         </p>
 
         <h1>Sponsored Research</h1>
         <ResearchPageEntry
           project="Nota Bene Annotated Textbook SuperUROP"
           date="August 2019-June 2020"
-          description={["Point 1", "Point 2"]}
+          description={[
+            "Spearheaded full-stack development of an anotated textbook website using Vue.js, Node.js, and PostgreSQL",
+            "Tested usability and effectiveness of website through user testing at UC Davis",
+            "Presented results of research through a series of presentations and a thesis",
+          ]}
         />
         <ResearchPageEntry
           project="MIT Quest for Intelligence UROP"
@@ -54,17 +68,25 @@ export default class Research extends Component {
         <ResearchPageEntry
           project="Kinect Gestural Classification"
           date="March-May 2020"
-          description={["Point 1", "Point 2"]}
+          description={[
+            "Created a web application that critiques a dancer's performance of pre-recorded dance choreography through use of Kinect skeleton tracking",
+            "Presented iterative models of prototype to peers and faculty throughout development",
+          ]}
         />
         <ResearchPageEntry
           project="Jobbies Web Application"
           date="November-December 2020"
-          description={["Point 1", "Point 2"]}
+          description={[
+            "Created an award-winning web application on a team of 4 which organizes a user's job applications including their position, company, status, and time between updates",
+            "Incorporated gmail api to filter a user's emails and allow them to attach recruiting emails to their job application objects",
+          ]}
         />
         <ResearchPageEntry
           project="Painterly Photo Rendering Project"
           date="May 2019"
-          description={["Point 1", "Point 2"]}
+          description={[
+            "Transformed digital photo renderings to resemble photo-realistic paintings with visible brush strokes",
+          ]}
         />
       </Container>
     );
